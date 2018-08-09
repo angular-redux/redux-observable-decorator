323af903a726e260d6d66f696e14784965c5ae99
Alexis Largaiolli <alexis.largaiolli.pro@gmail.com>
2018-08-09 17:12:13 +0200

    Angular v6 update
         * Update angular modules to v6
         * Update redux to v4
         * Update redux-observable to v1
         * Update rxjs to v6
         * Update createEpics to be comptatible with rxjs 6
         * createEpics now return a rootEpic that can be used in epicMiddleware.run(rootEpic); (https://redux-observable.js.org/docs/api/createEpicMiddleware.html)
         * Remove option support in createEpics method because option can be passed to epicMiddleware.run with root epic return by createEpics
         * Update unit tests

--- spec/create-epics.spec.ts
+++ spec/create-epics.spec.ts
@@ -1,40 +1,42 @@
-import { Epic, getEpicsMetadata, createEpics } from '../src/epic-decorator';
-import { createStore, applyMiddleware } from 'redux';
-import 'rxjs/add/operator/mapTo';
-import { createEpicMiddleware } from 'redux-observable';
+import { applyMiddleware, createStore } from 'redux';
+import { createEpicMiddleware, ofType } from 'redux-observable';
+import { createEpics, Epic } from '../src/epic-decorator';
+import { mapTo } from 'rxjs/operators';
+
 describe('createEpics', () => {
   it('should create an for a decorated method', () => {
     class Test {
-      @Epic() epic = (action$) => action$.ofType('TEST_IN').mapTo({ type: 'TEST_OUT' });
+      @Epic() epic = (action$) => action$.pipe(ofType('TEST_IN'), mapTo({ type: 'TEST_OUT' }));
     }
     const reducer = (state = [], action) => state.concat(action);
-    const epics = new Test();
-    const epicMiddleware = createEpics(epics);
+    const epic = new Test();
+    const epics = createEpics(epic);
+    const epicMiddleware = createEpicMiddleware();
     const store = createStore(reducer, applyMiddleware(epicMiddleware));
+    epicMiddleware.run(epics);
     const expected = [
-      { type: '@@redux/INIT' },
       { type: 'TEST_IN' },
       { type: 'TEST_OUT' }
     ];
     store.dispatch({ type: 'TEST_IN' });
-    const actual = store.getState();
-
+    let actual = store.getState();
+    actual.splice(0, 1); // Remove @@redux/INIT because its randomly generated 
     expect(actual).toEqual(expected);
-
   });
 
   it('should combine decorated methods into an epic', () => {
     class Test {
-      @Epic() epicOne = (action$) => action$.ofType('TEST_IN').mapTo({ type: 'TEST_OUT' });
-      @Epic() epicTwo = (action$) => action$.ofType('TEST_OUT').mapTo({ type: 'TEST_END' });
-      @Epic() epicThree = (action$) => action$.ofType('TEST_TWO').mapTo({ type: 'TEST_THREE' });
+      @Epic() epicOne = (action$) => action$.pipe(ofType('TEST_IN'), mapTo({ type: 'TEST_OUT' }));
+      @Epic() epicTwo = (action$) => action$.pipe(ofType('TEST_OUT'), mapTo({ type: 'TEST_END' }));
+      @Epic() epicThree = (action$) => action$.pipe(ofType('TEST_TWO'), mapTo({ type: 'TEST_THREE' }));
     }
     const reducer = (state = [], action) => state.concat(action);
-    const epics = new Test();
-    const epicMiddleware = createEpics(epics);
+    const epic = new Test();
+    const epics = createEpics(epic);
+    const epicMiddleware = createEpicMiddleware();
     const store = createStore(reducer, applyMiddleware(epicMiddleware));
+    epicMiddleware.run(epics);
     const expected = [
-      { type: '@@redux/INIT' },
       { type: 'TEST_IN' },
       { type: 'TEST_OUT' },
       { type: 'TEST_END' },
@@ -44,29 +46,31 @@ describe('createEpics', () => {
     store.dispatch({ type: 'TEST_IN' });
     store.dispatch({ type: 'TEST_TWO' });
     const actual = store.getState();
+    actual.splice(0, 1); // Remove @@redux/INIT because its randomly generated
 
     expect(actual).toEqual(expected);
   });
 
   it('should handle multipul classes', () => {
     class TestOne {
-      @Epic() a = (action$) => action$.ofType('TEST_A_IN').mapTo({ type: 'TEST_A_OUT' });
-      @Epic() b = (action$) => action$.ofType('TEST_B_IN').mapTo({ type: 'TEST_B_OUT' });
-      @Epic() c = (action$) => action$.ofType('TEST_C_IN').mapTo({ type: 'TEST_C_OUT' });
+      @Epic() a = (action$) => action$.pipe(ofType('TEST_A_IN'), mapTo({ type: 'TEST_A_OUT' }));
+      @Epic() b = (action$) => action$.pipe(ofType('TEST_B_IN'), mapTo({ type: 'TEST_B_OUT' }));
+      @Epic() c = (action$) => action$.pipe(ofType('TEST_C_IN'), mapTo({ type: 'TEST_C_OUT' }));
     }
     class TestTwo {
-      @Epic() d = (action$) => action$.ofType('TEST_D_IN').mapTo({ type: 'TEST_D_OUT' });
-      @Epic() e = (action$) => action$.ofType('TEST_E_IN').mapTo({ type: 'TEST_E_OUT' });
-      @Epic() f = (action$) => action$.ofType('TEST_F_IN').mapTo({ type: 'TEST_F_OUT' });
+      @Epic() d = (action$) => action$.pipe(ofType('TEST_D_IN'), mapTo({ type: 'TEST_D_OUT' }));
+      @Epic() e = (action$) => action$.pipe(ofType('TEST_E_IN'), mapTo({ type: 'TEST_E_OUT' }));
+      @Epic() f = (action$) => action$.pipe(ofType('TEST_F_IN'), mapTo({ type: 'TEST_F_OUT' }));
     }
 
     const reducer = (state = [], action) => state.concat(action);
     const epicOne = new TestOne();
     const epicTwo = new TestTwo();
-    const epicMiddleware = createEpics(epicOne, epicTwo);
+    const epics = createEpics(epicOne, epicTwo);
+    const epicMiddleware = createEpicMiddleware();
     const store = createStore(reducer, applyMiddleware(epicMiddleware));
+    epicMiddleware.run(epics);
     const expected = [
-      { type: '@@redux/INIT' },
       { type: 'TEST_A_IN' },
       { type: 'TEST_A_OUT' },
       { type: 'TEST_B_IN' },
@@ -89,82 +93,9 @@ describe('createEpics', () => {
     store.dispatch({ type: 'TEST_F_IN' });
 
     const actual = store.getState();
+    actual.splice(0, 1); // Remove @@redux/INIT because its randomly generated
 
     expect(actual).toEqual(expected);
   });
-
-  it('should pass in the options object if one is provided', () => {
-    class TestOneDep {
-      @Epic() a = (action$, store, deps) => action$
-        .ofType('TEST_A_IN')
-        .mapTo({ type: 'TEST_A_OUT', payload: deps.foo() });
-    }
-
-    class TestTwoDep {
-      @Epic() d = (action$, store, deps) => action$
-        .ofType('TEST_D_IN')
-        .mapTo({ type: 'TEST_D_OUT', payload: deps.foo() });
-
-    }
-
-    const reducer = (state = [], action) => state.concat(action);
-    const epicOne = new TestOneDep();
-    const epicTwo = new TestTwoDep();
-
-    const epicMiddleware = createEpics(epicOne, epicTwo, {
-      dependencies: {
-        foo: function () { return 'bar'; },
-      },
-    }, );
-
-    const store = createStore(reducer, applyMiddleware(epicMiddleware));
-
-    const expected = [
-      { type: '@@redux/INIT' },
-      { type: 'TEST_A_IN' },
-      { type: 'TEST_A_OUT', payload: 'bar' },
-      { type: 'TEST_D_IN' },
-      { type: 'TEST_D_OUT', payload: 'bar' },
-    ];
-
-    store.dispatch({ type: 'TEST_A_IN' });
-    store.dispatch({ type: 'TEST_D_IN' });
-
-
-    const actual = store.getState();
-
-    expect(actual).toEqual(expected);
-  });
-
-  it('should not pass in dependencies if no options provided', () => {
-    class TestOneNoDep {
-      @Epic() a = (action$, store, deps) => {
-        console.log(store, deps);
-        return action$.ofType('TEST_A_IN').mapTo({ type: 'TEST_A_OUT', payload: deps });
-      }
-    }
-
-    const reducer = (state = [], action) => state.concat(action);
-    const epicOne = new TestOneNoDep();
-
-
-    const epicMiddleware = createEpics(epicOne);
-
-    const store = createStore(reducer, applyMiddleware(epicMiddleware));
-
-    const expected = [
-      { type: '@@redux/INIT' },
-      { type: 'TEST_A_IN' },
-      { type: 'TEST_A_OUT', payload: undefined },
-
-    ];
-
-    store.dispatch({ type: 'TEST_A_IN' });
-
-
-    const actual = store.getState();
-    expect(actual).toEqual(expected);
-  });
-
 
 });
--- src/epic-decorator.ts
+++ src/epic-decorator.ts
@@ -1,16 +1,11 @@
-import { Action } from 'redux';
-import {
-  EpicMiddleware,
-  combineEpics,
-  createEpicMiddleware
-} from 'redux-observable';
+import { combineEpics, Epic } from 'redux-observable';
 const METADATA_KEY = 'redux-observable-decorator-metadata';
 
 export interface EpicMetadata {
   propertyName: string;
 }
 export function Epic(): PropertyDecorator {
-  return function(target: any, propertyName: string) {
+  return function (target: any, propertyName: string) {
     if (!(Reflect as any).hasOwnMetadata(METADATA_KEY, target)) {
       (Reflect as any).defineMetadata(METADATA_KEY, [], target);
     }
@@ -32,30 +27,14 @@ export function getEpicsMetadata(instance: any): EpicMetadata[] {
   return (Reflect as any).getOwnMetadata(METADATA_KEY, target);
 }
 
-function isOptions(...instanceOrOptions) {
-  let option = instanceOrOptions[instanceOrOptions.length - 1];
-  let keys = option ? Object.keys(option) : [];
-  return keys.indexOf('dependencies') >= 0 || keys.indexOf('adapter') >= 0;
-}
-export function createEpics<T extends Action, S>(
-  epic,
-  ...epicsOrOptions
-): EpicMiddleware<T, S> {
-  let instances;
-  let options;
-  if (isOptions(...epicsOrOptions)) {
-    options = epicsOrOptions.slice(
-      epicsOrOptions.length - 1,
-      epicsOrOptions.length
-    )[0];
-  }
-  instances = [epic, ...epicsOrOptions];
-
-  const epicsMetaData = instances.map(instance =>
+export function createEpics(
+  ...epics
+): Epic {
+  const epicsMetaData = epics.map(instance =>
     getEpicsMetadata(instance).map(({ propertyName }) => instance[propertyName])
   );
 
-  const epics = [].concat(...epicsMetaData);
-  const rootEpic = combineEpics<T, S>(...epics);
-  return createEpicMiddleware<T, S>(rootEpic, options);
+  const finalEpics = [].concat(...epicsMetaData);
+  const rootEpic = combineEpics(...finalEpics);
+  return rootEpic;
 }
